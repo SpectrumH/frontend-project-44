068b2c9e4451e6b99a016c0b1899bc5c417f3310
Gleb Kaplunskiy <gphenomen@gmail.com>
2023-09-23 15:23:32 +0300

    fix func call

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-};
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+const greeting = () => console.log(`Hello, ${name}!`);
+
+
+export { name };
+export default greeting;
