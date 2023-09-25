ec7223623080b43a98760feae73638432035898d
Gleb Kaplunskiy <gphenomen@gmail.com>
2023-09-25 10:29:55 +0300

    fix calc game logic

--- src/games/calc-index.js
+++ src/games/calc-index.js
@@ -7,24 +7,24 @@ const calc = (question) => {
     const arrOfOp = question.split(' ');
     const sign = ['+', '-', '*'];
     let result;
-    let temp;
-
-    for (let i = 0; i < arrOfOp.length; i += 1) {
+    let temp = Number(arrOfOp[0]);
+    for (let i = 1; i < arrOfOp.length; i += 1) {
         if (sign.includes(arrOfOp[i])) {
             switch (arrOfOp[i]) {
                 case '+':
                     result = temp + Number(arrOfOp[i + 1]);
+                    temp = result;
                     continue;
                 case '-':
                     result = temp - Number(arrOfOp[i + 1]);
+                    temp = result;
                     continue;
                 case '*':
                     result = temp * Number(arrOfOp[i + 1]);
+                    temp = result;
                     continue;
             }
-        } else {
-            temp = Number(arrOfOp[i]);
-        }
+        } 
     }
     return String(result);
 }
